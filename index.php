279cc27aa518c9da69cc12a04dcbee9ea41a52c7
MikeH <mike123.henry@gmail.com>
2017-01-08 16:18:28 -0600

    added .htaccess file and instantiated PHP micro framework -- slim

--- index.php
+++ index.php
@@ -3,13 +3,23 @@ require __DIR__ . '/vendor/autoload.php';
 
 date_default_timezone_set ( 'America/Chicago');
 
-
+//setup logger module
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-$log = new Logger('name');
-$log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
-$log->addWarning('Fooxxx');
+// $log = new Logger('name');
+// $log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
+// $log->addWarning('Fooxxx');
 
-echo 'Hello World!';
+//Instantiate a Slim application:
+$app = new \Slim\Slim();
+//Define a HTTP GET route
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+//run the app
+$app->run();
+
+
+
 ?>
