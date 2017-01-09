50f8ce5dff836619cc74fd0f65c09e5144fb1855
MikeH <mike123.henry@gmail.com>
2017-01-08 20:14:12 -0600

    switched to MVC model usng slim, twig and view

--- index.php
+++ index.php
@@ -1,21 +1,32 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require '/vendor/autoload.php';
+//require __DIR__ . '/vendor/autoload.php';
 
 date_default_timezone_set ( 'America/Chicago');
 
 //setup logger module
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
+//use Monolog\Logger;
+//use Monolog\Handler\StreamHandler;
 
 // $log = new Logger('name');
 // $log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
 // $log->addWarning('Fooxxx');
 
-//Instantiate a Slim application:
-$app = new \Slim\Slim();
+//Instantiate a Slim applicationn and use the Views\Twig switch
+$app = new \Slim\Slim(array(
+    'view' => new \Slim\Views\Twig()
+));
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true
+);
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension()
+);
+
 //Define a Home HTTP GET route
 $app->get('/', function () use($app) {
-    $app->render('index.html');
+    $app->render('about.twig');
 });
 
 //Define a contact HTTP GET route
