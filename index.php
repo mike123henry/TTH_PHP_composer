faa30abf88b1cc1dd0bcfd890453db51ff5807d3
MikeH <mike123.henry@gmail.com>
2017-01-17 15:05:47 -0600

    debugging autoloader path issue

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require '/vendor/autoload.php';
-//require __DIR__ . '/vendor/autoload.php';
+//require('/vendor/autoload.php');
+require __DIR__ . '/vendor/autoload.php';
 
 date_default_timezone_set ( 'America/Chicago');
 
